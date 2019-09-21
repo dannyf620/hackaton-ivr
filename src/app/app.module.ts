43af359108e0f022f22d0d576f57a6f893753619
Danny Mora <dannyf620@gmail.com>
2019-09-21 18:43:34 -0500

    add styles as a phone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { QuestionModule } from './modules/question/question.module';
+import { MatProgressSpinnerModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { QuestionModule } from './modules/question/question.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    QuestionModule
+    QuestionModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
